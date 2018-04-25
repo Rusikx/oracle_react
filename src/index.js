6c6f6257d6941676f5faa0207e60129da872f45e
den <wvsden@gmail.com>
2018-04-25 21:08:56 +0600

    Use mobx and start form

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import ViewPartsControl from "./storages/ViewPartsControl";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App viewControl={new ViewPartsControl} />, document.getElementById('root'));
 registerServiceWorker();
