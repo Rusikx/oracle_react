343463d6a6078b56ed780c69f3295b60f9d5f36c
Ruslan <dev5@prodengi.kz>
2018-06-02 19:24:39 +0600

    Ruslan - button controller

--- src/components/buttons/OracleButtonInput.js
+++ src/components/buttons/OracleButtonInput.js
@@ -1,14 +1,19 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
-import '../../styles/buttons/button.css';
+import '../../styles/buttons/button_input.css';
 
 class OracleButtonInput extends PureComponent {
 	render (){
 		return (
-			<div className={`button _input`}>
-				<input className="button__input" placeholder={this.props.title} type="tel" />
-				<div className="button__text">{this.props.title}</div>
+			<div onClick={()=>this.props.onClick()} className={this.props.action ? 'button _input _action' : 'button _input'}>
+				{(() => {
+					if(this.props.action){
+						return <input className="button__input" placeholder={this.props.title} type="tel" />;
+					}else{
+						return <div className="button__text">{this.props.title}</div>;
+					}
+				})()}
 			</div>
 		);
 	}
@@ -17,6 +22,8 @@ class OracleButtonInput extends PureComponent {
 OracleButtonInput.propTypes = {
 	title: PropTypes.string,
 	type: PropTypes.string,
+	action: PropTypes.bool,
+	onClick: PropTypes.func,
 };
 
 export default OracleButtonInput;
